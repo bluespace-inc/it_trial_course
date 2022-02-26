71ca3b713eece78bf8dfcf0a149c62daa16541b1
oononmaxq <oononmaxq@gmail.com>
2022-02-27 00:46:39 +0900

    intermediate 4.7 commit

--- Intermediate/python_lesson/project/app/views.py
+++ Intermediate/python_lesson/project/app/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from .models import Photo
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST) # Userインスタンスを作成
+    if form.is_valid():
+        form.save() # Userインスタンスを保存
+        input_username = form.cleaned_data['username']
+        input_password = form.cleaned_data['password1']
+        # フォームの⼊⼒値で認証できればユーザーオブジェクト、できなければNoneを返す
+        new_user = authenticate(
+            username=input_username,
+            password=input_password,
+        )
+        # 認証成功時のみ、ユーザーをログインさせる
+        if new_user is not None:
+            # login関数は、認証ができてなくてもログインさせることができる。(認証は上のauthenticateで実⾏する)
+            login(request, new_user)
+            return redirect('app:users_detail', pk=new_user.pk)
+        else:
+            form = UserCreationForm()
+        return render(request, 'app/signup.html', {'form': form})
+
 
 def index(request):
     # Photoインスタンスを全件取得
